cd2ed4a4ab10082368c38218a7e882e48849a3d3
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2021-11-17 11:54:57 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+//app modules
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+//functions
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  const options = {
+    dotfiles: "ignore",
+    etag: false,
+    extensions: ["htm", "html"],
+    index: false,
+    maxAge: "1d",
+    redirect: false,
+  };
+  app.use(express.static("client/build", options));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  console.log("Build is UP!");
+}
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const main = (async () => {
+  //no API
+})();
